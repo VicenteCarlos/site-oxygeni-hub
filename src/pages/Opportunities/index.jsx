bca7aab1eef3c6db51e1ae11fb919d542b59ca88
Matheus Farias Fernandes <matheus_farias_fernandes@outlook.com>
2022-05-02 16:57:55 -0300

    feat: Design Responsive in production | Oportunites

--- src/pages/Opportunities/index.jsx
+++ src/pages/Opportunities/index.jsx
@@ -10,14 +10,38 @@ export const Opportunities = () => (
         <section className="s-1">
           <img src="https://cdn.discordapp.com/attachments/651404729766772763/966410443159650444/Wavy_Bus-15_Single-01.jpg" alt="illustration" />
           <div className="icons">
-            <DiJsBadge className="js"/>
-            <DiJavascript className="html5"/>
-            <DiCss3 className="css3" />
-            <DiPython className="python3" />
-            <DiReact className="react" />
+            <section>
+                <DiJsBadge className="js"/>
+                <p class="FontSizeP">JavaScript</p>
+            </section>
+            <section>
+               <DiJavascript className="html5"/>
+               <p class="FontSizeP">HTML 5</p>
+            </section>
+           <section> 
+               <DiCss3 className="css3" />
+               <p class="FontSizeP">CSS3</p>  
+           </section>
+           <section> 
+              <DiPython className="python3" />
+              <p class="FontSizeP"> Python 3</p>
+           </section>
+           <section>
+              <DiReact className="react" />
+              <p class="FontSizeP">React</p>
+          </section>
+          <section>                
             <DiAngularSimple className="angular" />
+            <p class="FontSizeP"> Angular</p>
+         </section>
+         <section>   
             <DiMysql className="mysql"/>
+            <p class="FontSizeP">MySql</p>
+        </section>
+         <section>     
             <DiJava className="java" />
+            <p class="FontSizeP">Java</p>
+        </section>  
           </div>
         </section>
         <section className="s-2">
--- src/pages/Opportunities/style.js
+++ src/pages/Opportunities/style.js
@@ -2,16 +2,25 @@ import styled from "styled-components"
 
 export const StyledMain = styled.main`
   height: 850px;
-
+  width: 100vw;
+  /* margin-right:800px; */
+  
+  
 .container {
+  /* margin-right:800px;
+  margin-left:-180px; */
+  width: 200px;
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
-
+  flex-wrap:wrap;
+   
+   
+    
    h2 {
      color:#1c2f5d;
-     font-size: 50px;
+     font-size: 15px;
      text-align: center;
      margin-top: 70px;
      font-family: 'Plus Jakarta Sans', sans-serif;
@@ -19,78 +28,102 @@ export const StyledMain = styled.main`
 
    img {
      margin-top: 20px;
-     width: 600px;
+     width: 300px;
    }
 
    .s-1 {
        border-bottom: 15px dashed #1C2F5D;
        border-radius: 15px;
 
-        div {
+        .icons {
+            section{
+              display:flex;
+              flex-direction:column ;
+              align-items: center;
+              justify-content: center;
+            }
+
+          column-count: 2;
+          width: 300px;
+          display: flex;
+          flex-direction:row;
+          flex-wrap: wrap;
+
             .js {
             color: #F7DF1E;
-            width: 100px;
-            height: 150px;
+            width: 70px;
+            height: 100px;
+            font-size: small;
             }
 
             .html5 {
             color: #FF5722;
-            width: 100px;
-            height: 150px;
+            width: 70px;
+            height: 100px;
             }
 
             .css3 {
             color: #0070ba;
-            width: 100px;
-            height: 150px;
+            width: 70px;
+            height: 100px;
             }
 
             .python3 {
             color: #6B68CF;
-            width: 100px;
-            height: 150px;
+            width: 70px;
+            height: 100px;
             }
 
             .react {
             color: #5ccfee;
-            width: 100px;
-            height: 150px;
+            width: 70px;
+            height: 100px;
             }
 
             .angular {
             color: #d62f34;
-            width: 100px;
-            height: 150px;
+            width: 70px;
+            height: 100px;
             }
 
             .mysql {
-            width: 100px;
-            height: 150px;
+              width: 70px;
+            height: 100px;
             }
 
             .java {
             color: #F25C39;
-            width: 100px;
-            height: 150px;
+            width: 70px;
+            height: 100px;
+                      
+            }
+            .FontSizeP{
+              font-size: small;              
+              margin-bottom: 10px;
             }
         }
    }
 
    .s-2 {
         display: flex;
+        flex-direction: column;
         justify-content: space-between;
         align-items: center;
-
+        
+        
         img {
-            width: 700px;
-            height: 500px;
+            width: 400px;
+            height: 20px;
+            padding-left: 2px;
+            
         }
 
         p {
-            font-size: 30px;
+            font-size: 25px;
             color: #1c2f5d;
-            width: 400px;
+            width: 350px;
             text-align: center;
+            margin-right: 15px;
         }
    }
 }
