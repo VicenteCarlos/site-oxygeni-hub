482126916a2ef93d1eded62037ac022c22a50633
Elves Aguiar <elvesmhs@hotmail.com>
2022-05-14 19:18:33 -0300

    feat: implementing page Events

---
+++ src/pages/Events/style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+
+
+export const StyledMain = styled.main`
+
+
+ 
+
+  font-size: 62.5%;
+display: flex;
+flex-direction: column;
+align-items: center;
+height: auto;
+width: auto;
+margin: auto 20rem auto 20rem;
+
+
+
+
+
+
+
+`;
