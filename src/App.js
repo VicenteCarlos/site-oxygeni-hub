3cffb6c4e0d6febece269fe1f127b77d7dd701ce
Matheus Farias Fernandes <matheus_farias_fernandes@outlook.com>
2022-05-15 16:25:21 -0300

    feat:language buttons

--- src/App.js
+++ src/App.js
@@ -5,32 +5,13 @@ import { Footer } from "./components/Footer";
 import { useState } from "react";
 import Modal from "react-modal";
 
-Modal.setAppElement("#root");
+
 
 export const App = () => {
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-
-  // const [count, setCount] = useState(0);
-
-  // const handler = () => setCount(count + 1 );
-
   return (
     <>
-      {/* <button onClick={handler}>Counter:{count} </button> */}
-      <button onClick={() => setIsOpen(!modalIsOpen)}>Open Modal</button>
-      <Modal
-       isOpen={modalIsOpen}
-       OnRequestClose={() => setIsOpen(!modalIsOpen)}
-       contentLabel="Example Modal"
-       overlayClassName="modal-overlay"
-       className="modal-content"
-      >
-
-        <button onClick={() => setIsOpen(!modalIsOpen)}>Close modal</button>
-      </Modal>
-       <GlobalStyle />
-       {/* <Header />    */}
+      <GlobalStyle />
+      <Header />   
       <Rotas />
       <Footer /> 
     </>
